fbbd1208df8ab97e527298747dde6888e7fc530d
ADITYA <adityad@beepermd.com>
2025-07-01 13:54:30 +0000

    implement user authentication with JWT and update security configuration

--- user/src/main/java/org/backend/user/security/CustomAuthenticationFailureHandler.java
+++ user/src/main/java/org/backend/user/security/CustomAuthenticationFailureHandler.java
@@ -6,9 +6,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.backend.user.utils.SecurityUtils;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component
 public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
